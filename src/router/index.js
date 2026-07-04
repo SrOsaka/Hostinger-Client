1d822100aa896723107745cf6ee78bf34516246c
Gabryel Osaka <andersongabryel976@gmail.com>
2026-07-04 13:07:23 -0300

    fix: configure vue router and vite for github pages

--- src/router/index.js
+++ src/router/index.js
@@ -6,7 +6,7 @@ const routes = [
 ]
 
 const router = createRouter({
-    history: createWebHistory('/Hostinger-Client/'),
+    history: createWebHistory('import.meta.env.BASE_URL'),
     routes,
 })
 
