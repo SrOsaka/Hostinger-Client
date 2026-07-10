f67d0fa07fb57ea52386a771b5a020825891e845
Anderson Gabryel <andersongabryel976@gmail.com>
2026-07-10 09:43:51 -0300

    feat: add plans section card background
    Added a custom background design to the Plans section cards, improved the visual layout, and refined hover animations for a more polished interface.

---
+++ src/components/PlansSection.vue
@@ -0,0 +1,37 @@
+<script setup>
+    import OpenLinkIcon from './icons/OpenLinkIcon.vue';
+</script>
+<template>
+    <div class="plans-section-wrapper">
+        <div class="container text-center">
+            <div class="row">
+                <a href="" class="col plans-and-prices backgroundcardplans">
+                    <div class="tittle-card">
+                        <div>
+                            <h1>Plans and prices</h1>
+                            <OpenLinkIcon class="OpenLinkIcon" />
+                        </div>
+                        <p>Summer Sale: find the right plan and save up to 80%.</p>
+                    </div>
+                    <div class="plan-price-card">
+                        <h1>From <br> <b>$2.99</b>/month</h1>
+                    </div>
+                </a>
+                <a href="" class="col plans-and-prices">
+                    <div class="tittle-card">
+                        <div>
+                            <h1>Vibe code websites</h1>
+                            <OpenLinkIcon class="OpenLinkIcon" />
+                        </div>
+                        <p>Describe what you want, and Horizons builds it. <br> No technical skills needed.</p>
+                    </div>
+                    <div class="try-for-free">
+                        <h1>Try for free</h1>
+                        <p>No credit card required</p>
+                    </div>
+                </a> 
+            </div>
+        </div>
+    </div>
+</template>
+<style src="../assets/css/PlansSection.css" scoped></style>
--- src/views/index.vue
+++ src/views/index.vue
@@ -1,9 +1,11 @@
 <script setup> 
     import Navbar from '../components/Navbar.vue'
     import HomePageHero from '../components/HomePageHero.vue';
+    import PlansSection from '@/components/PlansSection.vue';
 </script>
 
 <template>
     <Navbar />
     <HomePageHero />
+    <PlansSection />
 </template>
